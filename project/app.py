661b482a294933c7f0c8f00308b9fca46770df2d
stardust-s <sage.jiang@mail.utoronto.ca>
2023-10-29 23:42:52 -0400

    Add very basic login

--- project/app.py
+++ project/app.py
@@ -1,9 +1,12 @@
 import sqlite3
 
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for
 
 # configuration
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "change_me"
 
 # create and initialize a new Flask app
 app = Flask(__name__)
@@ -42,6 +45,30 @@ def close_db(error):
     if hasattr(g, "sqlite_db"):
         g.sqlite_db.close()
 
+
+## ====== USER LOGIN ======
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """User login/authentication/session management."""
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    """User logout/authentication/session management."""
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
 
 ## ====== PAGES ======
 
